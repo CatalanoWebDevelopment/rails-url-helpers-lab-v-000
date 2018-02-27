32e21dd48167b6361011b7dc1d02a2b6547522b8
Parker Catalano <catalano.work@gmail.com>
2018-02-27 22:45:03 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
   def activate
     @student.update(active: !@student.active)
 
-    redirect student_path(@student)
+    redirect_to student_path(@student)
   end
 
   private
